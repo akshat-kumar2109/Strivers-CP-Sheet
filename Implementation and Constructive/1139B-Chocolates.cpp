3f3c731b7994c9463e5d56535a5c8527ed6be30b
akshat-kumar2109 <akshatkumar1101@gmail.com>
2022-10-04 12:37:55 +0530

    04-10-2022

--- Implementation and Constructive/1139B-Chocolates.cpp
+++ Implementation and Constructive/1139B-Chocolates.cpp
@@ -10,9 +10,28 @@ int32_t main() {
 	freopen("output.txt", "w", stdout);
 #endif
 
-	
+	int n;
+	cin>>n;
 
+	int arr[n];
+	for (int i=0; i<n; i++)
+		cin>>arr[i];
 
+	int curr_min = arr[n-1] - 1;
+	int res = arr[n-1];
+	for (int i = n - 2; i >= 0; i--) {
+		if (curr_min < 0) break;
+		// cout << curr_min << " " << res << endl;
+		if (arr[i] <= curr_min) {
+			curr_min = arr[i] - 1;
+			res += arr[i]; 
+		} else {
+			res += curr_min;
+			curr_min--;
+		}
+	}
 	
+	cout << res << endl;
+
 	return 0;
 }
